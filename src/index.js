85fbbe11b76f536a9daae0d5194a600021831976
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-08-04 14:33:57 +0100

    power save jquery

--- src/index.js
+++ src/index.js
@@ -7,5 +7,17 @@ $('#temperature-down').click(function(){
   thermostat.down();
   $('#temperature').text(thermostat.temperature);
 })
-
+$('#temperature-up').click(function(){
+  thermostat.up();
+  $('#temperature').text(thermostat.temperature)
+});
+$('#temperature-reset').click(function(){
+  thermostat.reset();
+  $('#temperature').text(thermostat.temperature)
+});
+$('#powersaving-off').click(function(){
+  thermostat.powerSaveOff();
+  $('#powerSave').text('off')
+  $('#temperature').text(thermostat.temperature)
+});
 });
