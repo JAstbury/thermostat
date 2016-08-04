9736be7d595ac21c105024d393e0755a73456a3e
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-08-04 15:52:23 +0100

    gets the weather

--- src/index.js
+++ src/index.js
@@ -1,31 +1,42 @@
 $(document).ready(function() {
 
+$('#select-city').submit(function(event) {
+  event.preventDefault();
+  var city = $('#current-city').val();
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  $('#current-temperature').text(data.main.temp);
+  })
+})
+
 var thermostat = new Thermostat();
-$('#temperature').text(thermostat.temperature);
+updateTemperature();
 
 $('#temperature-down').click(function(){
   thermostat.down();
-  $('#temperature').text(thermostat.temperature);
-})
+  updateTemperature();
+});
 $('#temperature-up').click(function(){
   thermostat.up();
-  $('#temperature').text(thermostat.temperature)
+  updateTemperature();
 });
 $('#temperature-reset').click(function(){
   thermostat.reset();
-  $('#temperature').text(thermostat.temperature)
+  updateTemperature();
 });
 
 $('#powersaving-off').click(function(){
   thermostat.powerSaveOff();
   $('#power-saving-status').text('off')
-  $('#temperature').text(thermostat.temperature)
+  updateTemperature();
 });
 
 $('#powersaving-on').click(function(){
   thermostat.powerSaveOn();
   $('#power-saving-status').text('on')
-  $('#temperature').text(thermostat.temperature)
+  updateTemperature();
 });
-
+function updateTemperature(){
+  $('#temperature').text(thermostat.temperature);
+  $('#temperature').attr('class', thermostat.energy());
+};
 });
