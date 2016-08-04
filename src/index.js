76b0fe0e4abfd3f51be1abf233935d1ec4f22395
JAstbury <jess.astbury@hotmail.com>
2016-08-04 14:13:47 +0100

    Adds start of jQuery

---
+++ src/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+var thermostat = new Thermostat();
+$('#temperature').text(thermostat.temperature);
+
+$('#temperature-down').click(function(){
+  thermostat.down();
+  $('#temperature').text(thermostat.temperature);
+})
+
+});
