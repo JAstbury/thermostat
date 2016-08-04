a00a79745d4bf7341e6b768fd2e473cc4fdbc8db
JAstbury <jess.astbury@hotmail.com>
2016-08-04 12:05:37 +0100

    Adds min and max temps

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -16,4 +16,27 @@ describe("thermostat", function(){
     thermostat.down()
     expect(thermostat.temperature).toBeLessThan(20)
   });
+  it("has a minimum temperature of 10 degrees", function(){
+    while(thermostat.temperature > 10){
+      thermostat.down();
+    }
+    expect(function(){thermostat.down();}).toThrowError('minimum temperature reached')
+  });
+  it("has power saving mode on by default", function(){
+    expect(thermostat.powerSave).toEqual(true);
+  })
+  it("has maximum temperature of 25 on power saving mode", function(){
+    while(thermostat.temperature < 25){
+      thermostat.up();
+    }
+    expect(function(){thermostat.up();}).toThrowError('maximum temperature reached')
+  })
+  it("has a maximum temperature of 32 if power saving mode is off", function(){
+    thermostat.powerSaveOff();
+    while(thermostat.temperature < 32){
+      thermostat.up();
+    }
+    expect(function(){thermostat.up();}).toThrowError('maximum temperature reached')
+  })
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,11 +1,30 @@
 Thermostat = function (){
-  this.temperature = 20
+  this.temperature = 20;
+  this._MINTEMP = 10;
+  this.powerSave = true;
 }
 
 Thermostat.prototype.up = function () {
+  if (this.powerSave === true && this.temperature >= 25) {
+    throw new Error('maximum temperature reached');
+  }
+  else if (this.powerSave == false && this.temperature >= 32) {
+    throw new Error('maximum temperature reached');
+  }
   this.temperature++
 };
 
 Thermostat.prototype.down = function () {
+  if (this.temperature <= this._MINTEMP) {
+    throw new Error('minimum temperature reached');
+  };
   this.temperature--
 };
+
+Thermostat.prototype.powerSaveOff = function() {
+  this.powerSave = false;
+}
+
+Thermostat.prototype.powerSaveOn = function() {
+  this.powerSave = true;
+}
